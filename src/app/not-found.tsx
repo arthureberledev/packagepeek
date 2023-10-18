d236e9d537772c3969664d2501c2eb7c9b8035bf
arthureberle <58264696+arthureberle@users.noreply.github.com>
2023-10-18 15:16:16 +0200

    add not-found file

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export const runtime = "edge";
+
+export default function NotFound() {
+  return (
+    <div className="text-center">
+      <p className="mt-6 text-base leading-7 text-stone-600">
+        Sorry, we couldn&apos;t find the page you&apos;re looking for.
+      </p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { SearchRepo } from "@/app/search-repo";
 import { searchReposAction } from "./actions";
 
+export const runtime = "edge";
+
 export default function Home() {
   return <SearchRepo searchReposAction={searchReposAction} />;
 }
